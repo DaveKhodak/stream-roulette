32d7e1a7ac54f3bffee16752f42a304310af9e54
davekhodak <david.khodak322@gmail.com>
2025-09-14 18:30:10 +0200

    add automatic migrations on startup

--- src/backend/stream-roulette.api/Program.cs
+++ src/backend/stream-roulette.api/Program.cs
@@ -1,7 +1,9 @@
+using Microsoft.EntityFrameworkCore;
 using stream_roulette.api.Services.Donations;
 using stream_roulette.core;
 using stream_roulette.core.Hubs;
 using stream_roulette.infra;
+using stream_roulette.infra.Persistence.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,12 @@ CoreDI.Configure(builder.Services, builder.Configuration);
 InfraDI.Configure(builder.Services, builder.Configuration);
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+    db.Database.Migrate();
+}
 
 app.UseCors(p => p.WithOrigins(builder.Configuration.GetValue<string>("Cors:WebUrl")!)
     .AllowAnyMethod()
